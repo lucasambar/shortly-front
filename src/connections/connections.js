f9de78367f15c0c5c1e074443445aa1bd58c49e3
Lucas <lucass.ambar@gmail.com>
2022-12-23 11:49:32 -0300

    feat: connectio to axios signin

--- src/connections/connections.js
+++ src/connections/connections.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export function signup (body) {
     return axios.post("https://shortly-keub.onrender.com/signup", body)
 }
+
+export function signin (body) {
+    return axios.post("https://shortly-keub.onrender.com/signin", body)
+}
--- src/pages/login/signin.js
+++ src/pages/login/signin.js
@@ -1,18 +1,32 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Forms from "../../components/forms";
 import Logo from "../../components/logo";
 import Topo from "../../components/topo";
+import { signin } from "../../connections/connections";
+import Loading from "../../constants/loading";
 
 export default function Signin () {
+    const [loading, setLoading] = useState(false)
+    const [erro, setErro] = useState("")
     const [user, setUser] = useState({
         email:"",
         password:""
     })
-    
+    const navigate = useNavigate()
+
+    function salvarUser (data) {
+        localStorage.setItem("user", JSON.stringify(data))
+        navigate("/home")
+    }
+
     function logar (event) {
         event.preventDefault();
+        setLoading(true)
 
-        console.log(user)
+        const promisse = signin(user)
+        promisse.then(res => salvarUser(res.data))
+        promisse.catch(erro => {setErro(erro.response.data); setLoading(false)})
     }
 
     return (
@@ -20,13 +34,16 @@ export default function Signin () {
             <Topo/>
             <Logo/>
             <Forms onSubmit={logar}>
-                <input placeholder="E-mail" value={user.email} type="email"
+                <input placeholder="E-mail" value={user.email} type="email" disabled={loading}
                 onChange={(e)=> setUser({...user, email: e.target.value})}/>
 
-                <input placeholder="Senha" value={user.password} type="password"
+                <input placeholder="Senha" value={user.password} type="password" disabled={loading}
                 onChange={(e)=> setUser({...user, password: e.target.value})}/>
 
-                <button type="submit"><p>Entrar</p></button>
+                <span>{erro}</span>
+                <button type="submit" disabled={loading}>
+                    {loading ? <Loading/> : <p>Entrar</p>}
+                </button>
             </Forms>
         </>
     )
